5fa2188314756457fb2e839c3590e1d405606f0b
Mahdi Rahman <35085896+mahdirahman1@users.noreply.github.com>
2024-10-09 23:13:05 -0400

    Time: 353 ms (44.22%) | Memory: 23.7 MB (56.38%) - LeetSync

---
+++ 1002-maximum-width-ramp/maximum-width-ramp.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxWidthRamp(self, nums: List[int]) -> int:
+        right_max = [nums[-1]] * len(nums)
+        for i in range(len(nums)-2, -1,-1):
+            right_max[i] = max(nums[i], right_max[i+1])
+        
+        res = 0
+        l = 0
+        for r in range(1, len(nums)):
+            left = nums[l]
+            if left > right_max[r]:
+                l += 1
+            else:
+                res = max(res, r-l)
+           
+        
+        
+        return res
