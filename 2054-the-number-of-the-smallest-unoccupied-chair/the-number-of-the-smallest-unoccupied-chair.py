e859ca0c0b6fecc6deff940a90b55176130ce47b
Mahdi Rahman <35085896+mahdirahman1@users.noreply.github.com>
2024-10-10 23:19:51 -0400

    Time: 541 ms (91.89%) | Memory: 22.7 MB (84.98%) - LeetSync

--- 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.py
+++ 2054-the-number-of-the-smallest-unoccupied-chair/the-number-of-the-smallest-unoccupied-chair.py
@@ -1,29 +1,22 @@
 class Solution:
     def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
         target_start = times[targetFriend][0]
-        start_to_end = defaultdict(int)
-        for start, end in times:
-            start_to_end[start] = end
-        
+        times.sort()
 
         seats = [i for i in range(len(times))]
         heapq.heapify(seats)
         event_heap = []
-
-        for time in range(target_start+1):
-            # see if any events end and add seat back
-            while event_heap and event_heap[0][0] == time:
+    
+        for start, end in times:
+            while event_heap and event_heap[0][0] <= start:
                 _, seat = heapq.heappop(event_heap)
                 heapq.heappush(seats, seat)
-                
-            if time == target_start:
-                return seats[0]
 
-            if time in start_to_end:
-                seat = heapq.heappop(seats)
-                heapq.heappush(event_heap, [start_to_end[time], seat])
-               
+            if start == target_start:
+                return seats[0]
+            
+            seat = heapq.heappop(seats)
+            heapq.heappush(event_heap, [end, seat])
+
         
-        print(seats)
-        return seats[0]
             
