d455745a8cd288207fe1b31e2d194839eb5779e7
Mahdi Rahman <35085896+mahdirahman1@users.noreply.github.com>
2024-10-23 21:06:55 -0400

    Time: 0 ms (100.00%) | Memory: 16.6 MB (57.29%) - LeetSync

--- 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.py
@@ -30,26 +30,17 @@ class Solution:
             if node1.left and node2.left:
                 if node1.left.val != node2.left.val:
                     # diff left values, keep node1, swap node2.left with node2.right
-                    left = dfs(node1.left, node2.right)
-                    right = dfs(node1.right, node2.left)
-                else:
-                    left = dfs(node1.left, node2.left)
-                    right = dfs(node1.right, node2.right)
+                    return dfs(node1.left, node2.right) and dfs(node1.right, node2.left)
 
             elif node2.right and node1.right:
                 if node1.right.val != node2.right.val:
-                    # diff left values, keep node1, swap node2.left with node2.right
-                    left = dfs(node1.left, node2.right)
-                    right = dfs(node1.right, node2.left)
-                else:
-                    left = dfs(node1.left, node2.left)
-                    right = dfs(node1.right, node2.right)
+                    # diff right values, keep node1, swap node2.left with node2.right
+                    return dfs(node1.left, node2.right) and dfs(node1.right, node2.left)
             else:
                 # one of them or both are None, swap node2.left with node2.right and try
-                left = dfs(node1.left, node2.right)
-                right = dfs(node1.right, node2.left)
+                return dfs(node1.left, node2.right) and dfs(node1.right, node2.left)
 
-            return left and right
+            return dfs(node1.left, node2.left) and dfs(node1.right, node2.right)
 
         return dfs(root1, root2)
             
