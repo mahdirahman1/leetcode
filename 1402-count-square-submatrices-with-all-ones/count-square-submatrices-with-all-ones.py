30d1b6b363df2dd1c8e03ed946302c9e9c00afc6
Mahdi Rahman <35085896+mahdirahman1@users.noreply.github.com>
2024-10-27 15:29:27 -0400

    Time: 255 ms (8.69%) | Memory: 31.4 MB (5.14%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.py
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.py
@@ -1,43 +1,39 @@
 class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
         res = 0
-        for i in range(len(matrix)):
-            sum_ = 0
-            for j in range(len(matrix[0])):
-                # count for 1x1
-                if matrix[i][j]:
-                    res += 1
-                
-                sum_ += matrix[i][j]
-                matrix[i][j] = sum_
+        cache = defaultdict(int)
 
-                # get the total ones for matrix ixj using prev sum in above row
-                if i > 0:
-                    matrix[i][j] += matrix[i-1][j]
-        
-        # now find all matrix from 2x2 onwards
-        # highest square matrix size can be min(m, n) = nxn
-        max_size = min(len(matrix), len(matrix[0]))
-
-        for size in range(2, max_size+1):
-            start = size-1
-            for x in range(size-1, len(matrix)):
-                for y in range(size-1, len(matrix[0])):
-                    # find matrix using (x,y)
-                    # if x - size exists, do matrix[x][y]-matrix[x-size][y]
-                    # if y - size exists, do matrix[x][y] - matrix[x][y-size]
-                    current = matrix[x][y]
-                    if x-size >= 0:
-                        current -= matrix[x-size][y]
-                    if y-size >= 0:
-                        current -= matrix[x][y-size]
-                    
-                    if x-size >=0 and y-size >= 0:
-                        current += matrix[x-size][y-size]
-                    
-                    if current == size * size:
-                        res += 1
-        
-        return res
+        def dfs(i,j, cache):
+            if (i,j) in cache:
+                return cache[(i,j)]
             
+                
+            # return 0 for subproblems out of bounds
+            if i >= len(matrix):
+                return 0
+            if j >= len(matrix[0]):
+                return 0
 
+            # if current value is 0, cant make subproblems so 0
+            if matrix[i][j] == 0:
+                cache[(i,j)] = 0
+                return 0
+
+            # solve the problem and subproblems from i,j
+            # check three sides of i,j
+            result = 1
+            result += min(dfs(i+1,j, cache), dfs(i+1, j+1, cache), dfs(i, j+1, cache))
+            
+            cache[(i,j)] = result
+            return result
+
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                res += dfs(i,j, cache)
+
+        return res
+                
+
+                
+      
